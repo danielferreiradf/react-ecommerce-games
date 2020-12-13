144a655ead8783a096a8b5619fa7a15dad103ba7
Daniel Ferreira da Silva <danielferreiradk@gmail.com>
2020-12-13 10:12:22 +0000

    Add next PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,12 +7,13 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avancado - Boilerplate</title>
+        <title>React Ecommerce</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="An ecommerce project to work with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
       <GlobalStyles />
